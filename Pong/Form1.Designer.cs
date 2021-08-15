fca4d691489c62a5457f6d98fa223bfa8d47cdd6
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-08-15 21:40:33 +0200

    Initial commit

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScorePlayerOne = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.PaddleTwo = new System.Windows.Forms.PictureBox();
+            this.PaddleOne = new System.Windows.Forms.PictureBox();
+            this.GameTimerP1 = new System.Windows.Forms.Timer(this.components);
+            this.GameTimerP2 = new System.Windows.Forms.Timer(this.components);
+            this.BallTimer = new System.Windows.Forms.Timer(this.components);
+            this.ScorePlayerTwo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PaddleTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PaddleOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScorePlayerOne
+            // 
+            this.ScorePlayerOne.AutoSize = true;
+            this.ScorePlayerOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScorePlayerOne.Location = new System.Drawing.Point(238, 9);
+            this.ScorePlayerOne.Name = "ScorePlayerOne";
+            this.ScorePlayerOne.Size = new System.Drawing.Size(27, 29);
+            this.ScorePlayerOne.TabIndex = 0;
+            this.ScorePlayerOne.Text = "0";
+            // 
+            // Ball
+            // 
+            this.Ball.Image = global::Pong.Properties.Resources.ball;
+            this.Ball.Location = new System.Drawing.Point(483, 304);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 3;
+            this.Ball.TabStop = false;
+            // 
+            // PaddleTwo
+            // 
+            this.PaddleTwo.Image = global::Pong.Properties.Resources.Paddle;
+            this.PaddleTwo.Location = new System.Drawing.Point(950, 245);
+            this.PaddleTwo.Name = "PaddleTwo";
+            this.PaddleTwo.Size = new System.Drawing.Size(22, 139);
+            this.PaddleTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PaddleTwo.TabIndex = 2;
+            this.PaddleTwo.TabStop = false;
+            // 
+            // PaddleOne
+            // 
+            this.PaddleOne.Image = global::Pong.Properties.Resources.Paddle;
+            this.PaddleOne.Location = new System.Drawing.Point(12, 245);
+            this.PaddleOne.Name = "PaddleOne";
+            this.PaddleOne.Size = new System.Drawing.Size(22, 139);
+            this.PaddleOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PaddleOne.TabIndex = 1;
+            this.PaddleOne.TabStop = false;
+            // 
+            // GameTimerP1
+            // 
+            this.GameTimerP1.Enabled = true;
+            this.GameTimerP1.Interval = 10;
+            this.GameTimerP1.Tick += new System.EventHandler(this.IngameTimer_P1);
+            // 
+            // GameTimerP2
+            // 
+            this.GameTimerP2.Enabled = true;
+            this.GameTimerP2.Interval = 10;
+            this.GameTimerP2.Tick += new System.EventHandler(this.InGameTimer_P2);
+            // 
+            // BallTimer
+            // 
+            this.BallTimer.Enabled = true;
+            this.BallTimer.Interval = 10;
+            this.BallTimer.Tick += new System.EventHandler(this.BallMovementTimer);
+            // 
+            // ScorePlayerTwo
+            // 
+            this.ScorePlayerTwo.AutoSize = true;
+            this.ScorePlayerTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScorePlayerTwo.Location = new System.Drawing.Point(704, 9);
+            this.ScorePlayerTwo.Name = "ScorePlayerTwo";
+            this.ScorePlayerTwo.Size = new System.Drawing.Size(27, 29);
+            this.ScorePlayerTwo.TabIndex = 4;
+            this.ScorePlayerTwo.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(984, 654);
+            this.Controls.Add(this.ScorePlayerTwo);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.PaddleTwo);
+            this.Controls.Add(this.PaddleOne);
+            this.Controls.Add(this.ScorePlayerOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PaddleMove);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressSpace);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PaddleStop);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PaddleTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PaddleOne)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ScorePlayerOne;
+        private System.Windows.Forms.PictureBox PaddleOne;
+        private System.Windows.Forms.PictureBox PaddleTwo;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer GameTimerP1;
+        private System.Windows.Forms.Timer GameTimerP2;
+        private System.Windows.Forms.Timer BallTimer;
+        private System.Windows.Forms.Label ScorePlayerTwo;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        bool goUpP1;
+        bool goDownP1;
+        bool goUpP2;
+        bool goDownP2;
+        bool ballGoesLeft;
+        int ballMovesUp;
+        int ballVerticalSpeed;
+        int ballRallyCount = 0;
+        int scorePOne = 0;
+        int scorePTwo = 0;
+        int paddleOneSpeed = 20;
+        int paddleTwoSpeed = 20;
+        int ballSpeed = 10;
+        Random random = new Random(); //randoms with only 2 act as bool
+
+        public Form1()
+        {
+            ballMovesUp = random.Next(2);
+            ballVerticalSpeed = random.Next(6);
+            InitializeComponent();
+            BallTimer.Enabled = false;
+        }
+
+        private void BallSpeedIncrease()
+        {
+            if (Ball.Bounds.IntersectsWith(PaddleOne.Bounds))
+            {
+                ballRallyCount++;
+            }
+            else if (Ball.Bounds.IntersectsWith(PaddleTwo.Bounds))
+            {
+                ballRallyCount++;
+            }
+
+            switch (ballRallyCount)
+            {
+                case 5:
+                    ballSpeed = 12;
+                    break;
+                case 10:
+                    ballSpeed = 14;
+                    break;
+                case 15:
+                    ballSpeed = 16;
+                    break;
+                case 20:
+                    ballSpeed = 18;
+                    break;
+                case 25:
+                    ballSpeed = 20;
+                    break;
+                case 30:
+                    ballSpeed = 22;
+                    break;
+                case 35:
+                    ballSpeed = 25;
+                    break;
+                case 40:
+                    ballSpeed = 30;
+                    break;
+            }
+        }
+
+        // Start Game
+
+        private void StartGame()
+        {
+            BallTimer.Enabled = true;
+            ballRallyCount = 0;
+            BallRandomizer();
+        }
+
+        private void PressSpace(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Space)
+            {
+                StartGame();
+            }
+        }
+
+        // Paddle Movement
+
+        private void PaddleMove(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                goUpP1 = true;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                goDownP1 = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUpP2 = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDownP2 = true;
+            }
+        }
+
+        private void PaddleStop(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                goUpP1 = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                goDownP1 = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUpP2 = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDownP2 = false;
+            }
+        }
+
+        // Ingame Timers
+
+        private void IngameTimer_P1(object sender, EventArgs e)
+        {
+            if (goUpP1)
+            {
+                PaddleOne.Top -= paddleOneSpeed;
+                if (PaddleOne.Top < -1)
+                {
+                    PaddleOne.Top += paddleOneSpeed;
+                }
+            }
+            else if (goDownP1)
+            {
+                PaddleOne.Top += paddleOneSpeed;
+                if (PaddleOne.Top > 517)
+                {
+                    PaddleOne.Top -= paddleOneSpeed;
+                }
+            }
+
+        }
+
+        private void InGameTimer_P2(object sender, EventArgs e)
+        {
+            if (goUpP2)
+            {
+                PaddleTwo.Top -= paddleTwoSpeed;
+                if (PaddleTwo.Top < -1)
+                {
+                    PaddleTwo.Top += paddleTwoSpeed;
+                }
+            }
+            else if (goDownP2)
+            {
+                PaddleTwo.Top += paddleTwoSpeed;
+                if (PaddleTwo.Top > 517)
+                {
+                    PaddleTwo.Top -= paddleTwoSpeed;
+                }
+            }
+        }
+
+        private void BallMovementTimer(object sender, EventArgs e)
+        {
+            if (ballGoesLeft)
+            {
+                BallMovementLeft();
+            }
+            else if (!ballGoesLeft)
+            {
+                BallMovementRight();
+            }
+
+            if (ballMovesUp == 1)
+            {
+                BallMovesUpwards();
+            }
+            else if (ballMovesUp == 0)
+            {
+                BallMovesDown();
+            }
+
+            BallSpeedIncrease();
+            ResetBall();
+            ScoreP1();
+            ScoreP2();
+        }
+
+
+        //Ball Movement
+
+        private void BallRandomizer()
+        {
+            if (random.Next(4) == 0)
+            {
+                ballGoesLeft = false;
+                ballMovesUp = 0;
+            }
+            else if (random.Next(4) == 1)
+            {
+                ballGoesLeft = true;
+                ballMovesUp = 1;
+            }
+            else if (random.Next(4) == 2)
+            {
+                ballGoesLeft = false;
+                ballMovesUp = 1;
+            }
+            else if (random.Next(4) == 3)
+            {
+                ballGoesLeft = true;
+                ballMovesUp = 0;
+            }
+
+            ballVerticalSpeed = random.Next(6);
+        }
+
+        private void BallMovesUpwards()
+        {
+            Ball.Top -= ballVerticalSpeed;
+            if (Ball.Top < 1)
+            {
+                Ball.Top += ballVerticalSpeed;
+            }
+        }
+
+        private void BallMovesDown()
+        {
+            Ball.Top += ballVerticalSpeed;
+            if (Ball.Top > 630)
+            {
+                Ball.Top -= ballVerticalSpeed;
+            }
+        }
+
+        private void BallMovementLeft()
+        {
+            Ball.Left -= ballSpeed;
+            if (Ball.Bounds.IntersectsWith(PaddleOne.Bounds))
+            {
+                ballVerticalSpeed = random.Next(6);
+                ballGoesLeft = false;
+                ballMovesUp = random.Next(2);
+            }
+        }
+
+        private void BallMovementRight()
+        {
+            Ball.Left += ballSpeed;
+            if (Ball.Bounds.IntersectsWith(PaddleTwo.Bounds))
+            {
+                ballVerticalSpeed = random.Next(6);
+                ballGoesLeft = true;
+                ballMovesUp = random.Next(2);
+            }
+        }
+
+        private void ResetBall()
+        {
+            if (Ball.Left > 1050 || Ball.Left < -20)
+            {
+                BallTimer.Enabled = false;
+                Ball.Left = 483;
+                Ball.Top = 304;
+            }
+        }
+
+        // Score
+
+        private void ScoreP1()
+        {
+            ScorePlayerOne.Text = scorePOne.ToString();
+            if (Ball.Left >= 1050)
+            {
+                scorePOne++;
+            }
+        }
+
+        private void ScoreP2()
+        {
+            ScorePlayerTwo.Text = scorePTwo.ToString();
+            if (Ball.Left <= -20)
+            {
+                scorePTwo++;
+            }
+        }
+    }
+}
